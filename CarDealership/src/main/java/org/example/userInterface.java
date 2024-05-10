a2d5bfae729b3e20847ab4fb1bb3a7b1cb6c521d
JNL001O <jlukusa@appdev.yearup.org>
2024-05-10 14:26:32 -0500

    CreateUserInterfaceClass

--- CarDealership/src/main/java/org/example/userInterface.java
+++ CarDealership/src/main/java/org/example/userInterface.java
@@ -7,17 +7,7 @@ public class userInterface {
    }
 
     public void displayOption(){
-        System.out.println("Welcome to the Dealership Application!");
-        System.out.println("1 - Find vehicles within a price range");
-        System.out.println("2 - Find vehicles by make / model");
-        System.out.println("3 - Find vehicles by year range");
-        System.out.println("4 - Find vehicles by color");
-        System.out.println("5 - Find vehicles by mileage range");
-        System.out.println("6 - Find vehicles by type");
-        System.out.println("7 - List all vehicles");
-        System.out.println("8 - Add a vehicle");
-        System.out.println("9 - Remove a vehicle");
-        System.out.println("99 - Quit");
+       throw new UnsupportedOperationException();
     }
 
     public void processGetByPriceRequest(){
