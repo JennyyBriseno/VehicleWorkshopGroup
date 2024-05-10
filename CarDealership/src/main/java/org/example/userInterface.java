c713d91db47e9a3afa3158e2791152f5a44437cd
JNL001O <jlukusa@appdev.yearup.org>
2024-05-10 13:40:04 -0500

    CreateUserInterfaceClass

---
+++ CarDealership/src/main/java/org/example/userInterface.java
@@ -0,0 +1,36 @@
+package org.example;
+import java.util.List;
+public class userInterface {
+   private Dealership dealership;
+   public userInterface(Dealership dealership){
+      this.dealership = dealership;
+   }
+
+    public void processGetByPriceRequest(){
+       throw new UnsupportedOperationException();
+    }
+    public void processGetByMakeModelRequest(){
+       throw new UnsupportedOperationException();
+    }
+    public void processGetByYearRequest(){
+       throw new UnsupportedOperationException();
+    }
+    public void processGetByColorRequest(){
+       throw new UnsupportedOperationException();
+    }
+    public void processGetByMileageRequest(){
+       throw new UnsupportedOperationException();
+    }
+    public void processGetByVehicleTypeRequest(){
+       throw new UnsupportedOperationException();
+    }
+    public void processGetAllVehicleRequest(){
+       throw new UnsupportedOperationException();
+    }
+    public void processAddVehicleRequest(){
+       throw new UnsupportedOperationException();
+    }
+    public void processRemoveVehicle(){
+       throw new UnsupportedOperationException();
+    }
+}
